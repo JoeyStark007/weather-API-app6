6b232674a068f136cee531d7274d623490957888
JoeyStark007 <jgtefera@gmail.com>
2022-11-22 16:26:01 -0500

    Added template html and flask navigation

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template
+import pandas as pd
 
+# Create website object
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    df = pd.read_csv("")
+    temperature = 21
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "  main  ":
+    app.run(debug=True)
