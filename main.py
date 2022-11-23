380a86441f0440893b1d3435a9e644aff4f38bf2
JoeyStark007 <84150889+JoeyStark007@users.noreply.github.com>
2022-11-23 04:32:47 -0500

    Show station table from csv file on webpage.

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import pandas as pd
 # Create website object
 app = Flask(__name__)
 
+stations = pd.read_csv("data/stations.txt", skiprows=17)
+# Keep columns we want to show
+stations = stations[["STAID", "STANAME                                 "]]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
