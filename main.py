cf1f43c3912b9712113f06969375c875a2ac4a01
JoeyStark007 <84150889+JoeyStark007@users.noreply.github.com>
2022-11-23 04:11:50 -0500

    Added txt files with weather info. Created interactive database that input link with station and date and outputs temperature

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    df = pd.read_csv("")
-    temperature = 21
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
 
-if __name__ == "  main  ":
+if __name__ == "__main__":
     app.run(debug=True)
